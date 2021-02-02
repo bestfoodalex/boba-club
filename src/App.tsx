bdb3552b81baa56b52871de4ad5a415e09dca0cb
Alex Jewell <al3xj3w3ll@gmail.com>
2021-02-02 01:00:09 -0600

    Basic structure of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Box, Button, Grommet, Image, Main } from 'grommet';
+import BobaTea from './images/tea.svg';
 
-function App() {
+const App = () => {
+  const [drink, setDrink] = useState('');
+
   return (
-    <section>
-      <article>
-        <header>
+    <Grommet full>
+      <Main pad="large">
+        <Header>
+          <BobaImage src={BobaTea} />
           <h1>Boba Club</h1>
-        </header>
-        <div>
-          <p>Generate your boba order!</p>
-        <div>
-      </article>
-    </section>
+        </Header>
+        <ButtonWrap pad="medium">
+          <Button size="large" primary label="Generate your boba order!" onClick={() => setDrink('Black milk tea with boba')} />
+        </ButtonWrap>
+        {drink !== '' && <ButtonWrap pad="medium">
+          <p>{drink}</p>
+        </ButtonWrap>}
+      </Main>
+    </Grommet>
   );
-}
+};
+
+const Header = styled.header`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const BobaImage = styled(Image)`
+  height: auto;
+  width: 45px;
+`;
+
+const ButtonWrap = styled(Box)`
+  text-align: center;
+`;
 
 export default App;
